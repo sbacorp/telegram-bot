097b5d7d53960c9b135709ee51ad803808aa2b06
Bogdan <98741979+sbacorp@users.noreply.github.com>
2023-12-19 12:16:47 +0300

    12123

--- src/server/database.ts
+++ src/server/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 import * as pg from "pg";
 import { config } from "#root/config.js";
 
-export const sequelize = new Sequelize("tgbotDB", "bogdan", "root", {
+export const sequelize = new Sequelize("tgbotDB", "user", "root", {
   host: config.DATABASE_IP,
   port: 5432,
   dialect: "postgres",
